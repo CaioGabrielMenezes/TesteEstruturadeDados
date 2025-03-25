9f388ee0eab0181199e6824a49c3d03d5b8ac08e
CaioGabrielMenezes <99057201+CaioGabrielMenezes@users.noreply.github.com>
2025-03-25 19:48:36 -0300

    exercicio2_fila_simulacao.py

--- exercicio2_fila_simulacao.py
+++ exercicio2_fila_simulacao.py
@@ -1,10 +1,22 @@
-from collections import deque
+class Fila:
+    def __init__(self):
+        self.itens = []
 
-fila = deque()
+    def adicionar(self, item):
+        self.itens.append(item)
 
-fila.append(1)
+    def remover(self):
+        if self.item:
+            return self.item.pop(0)
+        return None
 
-fila.append(2)
+    def vazia(self):
+        return len(self.items) == 0
 
-if fila:
-    print(fila.popleft())
+
+fila = Fila()
+fila.adicionar(1)
+fila.adicionar(2)
+
+if not fila.vazia():
+    print(fila.remover())
