98d8c18baad22ae1e4085703400e5fa00de7e263
CaioGabrielMenezes <99057201+CaioGabrielMenezes@users.noreply.github.com>
2025-03-25 19:55:17 -0300

    exercicio3_fila_reversao.py

--- exercicio3_fila_reversao.py
+++ exercicio3_fila_reversao.py
@@ -1,12 +1,25 @@
-from collections import deque
+class Fila:
+    def __init__(self):
+        self.itens = []
 
-fila = deque()
+    def adicionar(self,item):
+        self.itens.append(item)
 
-fila.append(1)
-fila.append(2)
-fila.append(3)
+    def remover(self):
+        if self.itens:
+            return self.itens.pop(0)
+        return None
 
-fila.reverse()
+    def vazia(self):
+        return len(self.itens) == 0
 
-for elemento in fila:
-    print(elemento)
+    def inversao(self):
+        for item in self.itens[::-1]:
+            print(item)
+
+fila = Fila()
+fila.adicionar(1)
+fila.adicionar(2)
+fila.adicionar(3)
+
+fila.inversao()
